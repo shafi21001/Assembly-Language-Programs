f700085f87751d3636156076cabdfa32fa4ce9dc
Md. Shafi Mahmud <shafi.msm1999@gmail.com>
2025-01-15 19:36:23 +0600

    Update AND, OR XOR .asm

--- AND, OR XOR .asm
+++ AND, OR XOR .asm
@@ -1,14 +1,11 @@
- .model small
+.model small
 .stack 100h
 .data      
  a db 10,13, 'For AND :$'
  b db 10,13, 'for OR : $'
  c db 10,13, 'for XOR : $' 
-  
-
 .code
 main proc
-
   mov ax,@data
   mov ds,ax
   
@@ -27,7 +24,7 @@ main proc
   
   level_or:
   
-    mov ah,9
+  mov ah,9
   lea dx,b
   int 21h
   
@@ -43,7 +40,7 @@ main proc
   
   level_xor: 
   
-    mov ah,9
+  mov ah,9
   lea dx,c
   int 21h
   
@@ -57,9 +54,7 @@ main proc
   int 21h
   
   exit:
-  
   mov ah,4ch
   int 21h
-  
   main endp
 end main
